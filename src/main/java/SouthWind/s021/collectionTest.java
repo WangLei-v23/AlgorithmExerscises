47af846a30b114f1b98d0788c215dcfe1fbb8da6
WangLei <2466682332@qq.com>
2021-06-30 02:03:43 +0800

    集合框架demo(其实就是实例化之后看了看源码。。)

---
+++ src/main/java/SouthWind/s021/collectionTest.java
@@ -0,0 +1,15 @@
+package SouthWind.s021;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class collectionTest {
+    public static void main(String[] args) {
+        //查看集合源码
+        ArrayList arrayList = new ArrayList();
+        Vector vector = new Vector();
+        LinkedList linkedList = new LinkedList();
+    }
+}
