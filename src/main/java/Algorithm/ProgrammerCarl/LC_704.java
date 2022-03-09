b85f3282b517aa7aef5b5b361b3eb5bdd98cc1bd
WangLei <2466682332@qq.com>
2022-03-09 13:58:22 +0800

    新增：carl-数组-t1

---
+++ src/main/java/Algorithm/ProgrammerCarl/LC_704.java
@@ -0,0 +1,14 @@
+package Algorithm.ProgrammerCarl;
+
+public class LC_704 {
+
+    public  int search(int[] nums, int target) {
+        //while循环收敛即可 注意因边界不同产生的两种写法
+        //1 [left,right]  2 [left,right)
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
